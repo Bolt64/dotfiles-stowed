1aff578703ac496d43b3d76e30a3bddc9b09622e
Sayantan Khan <bolt.khan@gmail.com>
2019-01-19 23:31:33 -0500

    updated backup method

--- bin/bin/borg_backup.sh
+++ bin/bin/borg_backup.sh
@@ -1,21 +1,69 @@
 #!/bin/sh
-REPOSITORY=/run/media/bolt/aaedbdd8-a511-4c01-bac6-8c41f582d94b/Backups
 
-borg create -v --progress --compression lzma,5      \
-            --stats                                 \
-    $REPOSITORY::'{hostname}-{now:%Y-%m-%d-%s}'     \
-    /home/bolt                                      \
-    --exclude /home/bolt/.cache                     \
-    --exclude /home/bolt/Music                      \
-    --exclude /home/bolt/Videos                     
-    #--exclude /home/bolt/.mozilla                   \
-    #--exclude /home/bolt/.config/chromium-backup    \
-    #--exclude /home/bolt/.config/chromium-back-ovfs \
-    #--exclude /home/bolt/temp                       \
+# Setting this, so the repo does not need to be given on the commandline:
+export BORG_REPO=/run/media/sayantan/aaedbdd8-a511-4c01-bac6-8c41f582d94b/Archives/Backups-arch-thinkpad
+
+# Setting this, so you won't be asked for your repository passphrase:
+export BORG_PASSPHRASE='passphrase'
+# or this to ask an external program to supply the passphrase:
+# export BORG_PASSCOMMAND='pass show backup'
+
+# some helpers and error handling:
+info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
+trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
+
+info "Starting backup"
+
+# Backup the most important directories into an archive named after
+# the machine this script is currently running on:
+
+borg create                         \
+    --verbose                       \
+    --filter AME                    \
+    --list                          \
+    --stats                         \
+    --show-rc                       \
+    --compression lz4               \
+    --exclude-caches                    \
+    --exclude '/home/*/.cache/*'        \
+    --exclude '/var/cache/*'            \
+    --exclude '/cryptpart/lost+found/'  \
+    --exclude '/var/tmp/*'          \
+				    \
+    ::'{hostname}-{now}'            \
+    /home/sayantan                           \
+    /cryptpart                           \
+
+backup_exit=$?
+
+info "Pruning repository"
 
 # Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
 # archives of THIS machine. The '{hostname}-' prefix is very important to
 # limit prune's operation to this machine's archives and not apply to
-# other machine's archives also.
-borg prune -v --list $REPOSITORY --prefix '{hostname}-' \
-    --keep-daily=100 --keep-weekly=50 --keep-monthly=20
+# other machines' archives also:
+
+borg prune                          \
+    --list                          \
+    --prefix '{hostname}-'          \
+    --show-rc                       \
+    --keep-daily    7               \
+    --keep-weekly   4               \
+    --keep-monthly  6               \
+
+prune_exit=$?
+
+# use highest exit code as global exit code
+global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
+
+if [ ${global_exit} -eq 1 ];
+then
+    info "Backup and/or Prune finished with a warning"
+fi
+
+if [ ${global_exit} -gt 1 ];
+then
+    info "Backup and/or Prune finished with an error"
+fi
+
+exit ${global_exit}
